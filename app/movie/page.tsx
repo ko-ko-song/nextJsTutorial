91a58f31c32a1b18bdb8bd583be4a6f3821bd7db
Song <ssb00161@naver.com>
2024-07-10 22:31:15 +0900

    3.3

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-function Movie() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{isLoading ? "loading... " : JSON.stringify(movies, null, 2)}</div>;
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
 }
 
-export default Movie;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+}
